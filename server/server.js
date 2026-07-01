48c2d0aa6dbf40fd82c1cd4c72e517bb6d5c955b
Alexsabu <alexsabu053@gmail.com>
2026-07-01 20:31:09 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -3,13 +3,20 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
 
+const authRoutes = require("./routes/authRoutes");
+const taskRoutes = require("./routes/taskRoutes");
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+// Routes
+app.use("/api/auth", authRoutes);
+app.use("/api/tasks", taskRoutes);
+
 app.get("/", (req, res) => {
-  res.send("Welcome to Task Manager API");
+  res.send("Welcome to MERN Task Manager API");
 });
 
 mongoose
@@ -18,7 +25,7 @@ mongoose
     console.log("MongoDB Connected");
 
     app.listen(process.env.PORT || 5000, () => {
-      console.log("Server Running");
+      console.log("Server Running on Port 5000");
     });
   })
   .catch((err) => console.log(err));
